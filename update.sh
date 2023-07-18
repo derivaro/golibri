2ba28558a95a150edb786ff0387cfd2691e8e0fa
Antoine <antoine@wizebulk.com>
2023-07-18 18:14:47 +0200

    update v0.1.28

--- update.sh
+++ update.sh
@@ -1,3 +1,20 @@
 
-.gitpush $1
-./.register $1
+#!/bin/sh
+##gitpush.sh
+if [ "$1" = "" ]; then
+echo "no message"
+else
+git add *
+git add .
+git commit -m "update $1"
+git tag $1
+git push origin $1
+
+git push origin head
+go list -m github.com/derivaro/golibri@$1
+echo "done"
+fi
+
+
+
+#./.register $1
